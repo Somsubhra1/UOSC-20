004726743125db118f94c746f7a28eaab10c2b29
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2021-11-21 22:09:36 +0530

    fix: missing quotes in alt

--- assets/js/eventFetch.js
+++ assets/js/eventFetch.js
@@ -10,12 +10,12 @@ function getCoordinators(coordinators) {
           <span>
             <strong>${coordinator.name}</strong>:
 
-            <a href="tel:${coordinator.phone}" class="text-dark" >
-            <img src="https://ureckon-21.herokuapp.com/assets/events/phone.svg" style="height: 1.2rem;width: 1rem;" alt="email/>
+            <a href="tel:${coordinator.phone}" class="text-dark">
+            <img src="https://ureckon-21.herokuapp.com/assets/events/phone.svg" style="height: 1.2rem;width: 1rem;" alt="tel"/>
             </a>
             
             <a href="mailto:${coordinator.email}" class="text-dark">
-            <img src="https://ureckon-21.herokuapp.com/assets/events/email.svg" style="height: 1.2rem;width: 1rem;" alt="tel"/>
+            <img src="https://ureckon-21.herokuapp.com/assets/events/email.svg" style="height: 1.2rem;width: 1rem;" alt="email"/>
             </a>
             </span>
         </li>
