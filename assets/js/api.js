af8717d0e75682924b324328f52c5e4d845ad1e8
Prasun Das <58937669+Prasundas99@users.noreply.github.com>
2021-11-21 20:39:17 +0530

    change api base url

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,1 +1,2 @@
-const baseURL = 'https://ureckon-api-dev-spn77.ondigitalocean.app'
+//const baseURL = 'https://ureckon-api-dev-spn77.ondigitalocean.app'
+const baseURL = 'https://ureckon-2022-backend.herokuapp.com'
